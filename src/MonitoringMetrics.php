3d098c8de3a84858e6bae900117fd1e9a02258ef
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2024-07-20 12:19:38 +0700

    Adding function to controller ..

--- src/MonitoringMetrics.php
+++ src/MonitoringMetrics.php
@@ -29,8 +29,9 @@ class MonitoringMetrics
         $action = 'click';
         $method = 'GET';
         $project_id = $this->projectID;
+        $function = 'trackClick()';
 
-        $get_traffic = $this->_getDataTraffic($action, $method, $project_id);
+        $get_traffic = $this->_getDataTraffic($action, $method, $project_id, $function);
         
         $this->sendData($action, $get_traffic);
     }
@@ -40,8 +41,9 @@ class MonitoringMetrics
         $action = 'view';
         $method = 'GET';
         $project_id = $this->projectID;
+        $function = 'trackPageView()';
         
-        $get_traffic = $this->_getDataTraffic($action, $method, $project_id);
+        $get_traffic = $this->_getDataTraffic($action, $method, $project_id, $function);
 
         $this->sendData($action, $get_traffic);
     }
@@ -51,8 +53,9 @@ class MonitoringMetrics
         $action = 'download';
         $method = 'GET';
         $project_id = $this->projectID;
+        $function = 'trackDownload()';
 
-        $get_traffic = $this->_getDataTraffic($action, $method, $project_id);
+        $get_traffic = $this->_getDataTraffic($action, $method, $project_id, $function);
 
         $this->sendData($action, $get_traffic);
     }
@@ -62,8 +65,9 @@ class MonitoringMetrics
         $action = 'create';
         $method = 'GET';
         $project_id = $this->projectID;
+        $function = 'trackCreate()';
 
-        $get_traffic = $this->_getDataTraffic($action, $method, $project_id);
+        $get_traffic = $this->_getDataTraffic($action, $method, $project_id, $function);
         
         $this->sendData($action, $get_traffic);
     }
@@ -73,8 +77,9 @@ class MonitoringMetrics
         $action = 'save';
         $method = 'POST';
         $project_id = $this->projectID;
+        $function = 'trackSave()';
 
-        $get_traffic = $this->_getDataTraffic($action, $method, $project_id);
+        $get_traffic = $this->_getDataTraffic($action, $method, $project_id, $function);
         
         $this->sendData($action, $get_traffic);
     }
@@ -84,8 +89,9 @@ class MonitoringMetrics
         $action = 'reports';
         $method = 'GET';
         $project_id = $this->projectID;
+        $function = 'trackReports()';
 
-        $get_traffic = $this->_getDataTraffic($action, $method, $project_id);
+        $get_traffic = $this->_getDataTraffic($action, $method, $project_id, $function);
         
         $this->sendData($action, $get_traffic);
     }
@@ -95,8 +101,9 @@ class MonitoringMetrics
         $action = 'edit';
         $method = 'GET';
         $project_id = $this->projectID;
+        $function = 'trackEdit()';
 
-        $get_traffic = $this->_getDataTraffic($action, $method, $project_id);
+        $get_traffic = $this->_getDataTraffic($action, $method, $project_id, $function);
 
         $this->sendData($action, $get_traffic);
     }
@@ -106,8 +113,9 @@ class MonitoringMetrics
         $action = 'update';
         $method = 'POST';
         $project_id = $this->projectID;
+        $function = 'trackUpdate()';
 
-        $get_traffic = $this->_getDataTraffic($action, $method, $project_id);
+        $get_traffic = $this->_getDataTraffic($action, $method, $project_id, $function);
 
         $this->sendData($action, $get_traffic);
     }
@@ -117,8 +125,9 @@ class MonitoringMetrics
         $action = 'delete';
         $method = 'POST';
         $project_id = $this->projectID;
+        $function = 'trackDelete()';
 
-        $get_traffic = $this->_getDataTraffic($action, $method, $project_id);
+        $get_traffic = $this->_getDataTraffic($action, $method, $project_id, $function);
 
         $this->sendData($action, $get_traffic);
     }
@@ -133,7 +142,7 @@ class MonitoringMetrics
         return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
     }
 
-    public function _getDataTraffic($action, $method, $project_id)
+    public function _getDataTraffic($action, $method, $project_id, $function)
     {
         $data = [
             'timestamp'         => date('Y-m-d H:i:s'),
@@ -142,32 +151,15 @@ class MonitoringMetrics
             'action'            => $action,
             'activity_url'      => url()->current(),
             'ip_address'        => $this->get_client_ip(),
+            'app_host'          => $this->detectDevice(),
+            'app_browser'       => $this->detectBrowser(),
+            'function'          => $function,
+            'headers'           => $this->_getHeaders(),
+            'data_request'      => $this->_getRequest(),
+            'data_response'     => $this->_getResponse(),
         ];
 
         return $data;
-    }
-
-    public function get_client_ip() {
-        
-        $ipaddress = '';
-        
-        if (isset($_SERVER['HTTP_CLIENT_IP'])) {
-            $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
-        } else if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-            $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
-        } else if (isset($_SERVER['HTTP_X_FORWARDED'])) {
-            $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
-        } else if (isset($_SERVER['HTTP_FORWARDED_FOR'])) {
-            $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
-        } else if (isset($_SERVER['HTTP_FORWARDED'])) {
-            $ipaddress = $_SERVER['HTTP_FORWARDED'];
-        } else if (isset($_SERVER['REMOTE_ADDR'])) {
-            $ipaddress = $_SERVER['REMOTE_ADDR'];
-        } else {
-            $ipaddress = 'UNKNOWN';
-        }
-
-        return $ipaddress;
     }
 
     public function _getBaseAPI($type, $value)
@@ -215,5 +207,138 @@ class MonitoringMetrics
         curl_close($ch);
 
         return $statusCode;
+    }
+
+    public function get_client_ip() {
+        
+        $ipaddress = '';
+        
+        if (isset($_SERVER['HTTP_CLIENT_IP'])) {
+            $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+        } else if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        } else if (isset($_SERVER['HTTP_X_FORWARDED'])) {
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+        } else if (isset($_SERVER['HTTP_FORWARDED_FOR'])) {
+            $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+        } else if (isset($_SERVER['HTTP_FORWARDED'])) {
+            $ipaddress = $_SERVER['HTTP_FORWARDED'];
+        } else if (isset($_SERVER['REMOTE_ADDR'])) {
+            $ipaddress = $_SERVER['REMOTE_ADDR'];
+        } else {
+            $ipaddress = 'UNKNOWN';
+        }
+
+        return $ipaddress;
+    }
+
+    public function detectDevice() {
+        $userAgent = $_SERVER['HTTP_USER_AGENT'];
+        $deviceType = "Unknown Device";
+
+        // Define regex patterns for different device types
+        $devicePatterns = [
+            'Mobile'    => '/mobile|android|kindle|silk|opera mini|opera mobi|blackberry|webos|windows phone|phone|pocket|palm|cricket|docomo|fone/i',
+            'Tablet'    => '/tablet|ipad|playbook|silk/i',
+            'Desktop'   => '/macintosh|windows|linux|x11/i',
+            'Bot'       => '/bot|crawl|slurp|spider|mediapartners|adsbot|bingbot|googlebot|yandexbot/i',
+        ];
+
+        // Loop through patterns and match against the User-Agent
+        foreach ($devicePatterns as $device => $pattern) {
+            if (preg_match($pattern, $userAgent)) {
+                $deviceType = $device;
+                break;
+            }
+        }
+
+        return $deviceType;
+    }
+
+    public function detectBrowser() {
+        $userAgent = $_SERVER['HTTP_USER_AGENT'];
+        $browser = "Unknown Browser";
+
+        // Define regex patterns for different browsers
+        $browserPatterns = [
+            'Edge'              => '/edge/i',
+            'Internet Explorer' => '/msie|trident/i',
+            'Opera'             => '/opera|opr/i',
+            'Firefox'           => '/firefox/i',
+            'Chrome'            => '/chrome|crios/i',
+            'Safari'            => '/safari/i',
+        ];
+
+        // Loop through patterns and match against the User-Agent
+        foreach ($browserPatterns as $name => $pattern) {
+            if (preg_match($pattern, $userAgent)) {
+                $browser = $name;
+                break;
+            }
+        }
+
+        return $browser;
+    }
+
+    public function _getHeaders()
+    {
+        $headers = [
+            'Authorization: Bearer '.$this->apiToken,
+            'Content-Type: application/json',
+        ];
+
+        return json_encode($headers);
+    }
+
+    public function _getRequest()
+    {
+        $ch = curl_init($this->apiUrl.'track');
+
+        $data = [
+            'type' => 'tracking',
+        ];
+
+        $headers = [
+            'Authorization: Bearer '.$this->apiToken,
+            'Content-Type: application/json',
+        ];
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_VERBOSE, true);  // Enable verbose output
+        curl_setopt($ch, CURLOPT_HEADER, true);   // Include headers in output
+
+
+        $info = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+
+        return json_encode($info);
+    }
+
+    public function _getResponse()
+    {
+        $ch = curl_init($this->apiUrl.'track');
+
+        $data = [
+            'type' => 'tracking',
+        ];
+
+        $headers = [
+            'Authorization: Bearer '.$this->apiToken,
+            'Content-Type: application/json',
+        ];
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_VERBOSE, true);  // Enable verbose output
+        curl_setopt($ch, CURLOPT_HEADER, true);   // Include headers in output
+
+
+        $info = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+
+        return json_encode($info);
     }
 }
